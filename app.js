2c3e9854ba59eac33a56c5e5c93743de42f57c66
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-11-30 16:27:21 +0200

    # build basic server

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-/* write your server code here */
+const express = require('express');
+const app = express();
+// const cors = require('cors');
+
+// app.use(cors({ origin: '*' }));
+app.use(express.json());
+
+// app.use(express.static(`./client/build`));
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/client/build/index.html');
+// });
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-/* write the code to run app.js here */
+const app = require('./app');
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => console.log(`app listening at http://localhost:${PORT}`));
