4ce5f91742bb8a502222584c783c9ad93f954105
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-11-30 19:36:16 +0200

    # Add semiDB of USRES, INFORMATION, REFRESHTOKENS

---
+++ backEnd/data/users.js
@@ -0,0 +1,18 @@
+/***** CONSTANTS *****/
+
+// contain objects - {email, name, password, isAdmin}
+const USERS = [
+  {
+    email: 'admin@email.com',
+    name: 'admin',
+    password: '$2b$10$kdXzMfLXkURVha/oO0stTOJO1k8cxuJnrfPao3nvIN.tsfHdEKyEy!',
+    isAdmin: true,
+  },
+];
+
+// contain objects - {email, info}
+const INFORMATION = [];
+
+const REFRESHTOKENS = [];
+
+module.exports = { USERS, INFORMATION, REFRESHTOKENS };
