c366346d313e91431e280e4255fe426e0f91cb9a
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-12-01 01:02:36 +0200

    # "api/v1/users" endpoint - Get users DB (admin only) by access token header

--- backEnd/controller/api.js
+++ backEnd/controller/api.js
@@ -1,8 +1,19 @@
 const { USERS, INFORMATION, REFRESHTOKENS } = require('../data/users.js');
 
-/********** Authorized User **********/
+/********** Authorized User + GET DATA **********/
 //Providing information to an authorized user about himself
 exports.getInfo = (req, res) => {
   const { user } = req;
   res.status(200).send([INFORMATION.find((info) => info.email === user.email)]);
+};
+
+// Get all users DB if admin users
+exports.getUsersDB = (req, res) => {
+  const { isAdmin } = req.user; //Get valid user isAdmin value
+
+  if (!isAdmin) {
+    return res.status(403).send('Invalid Access Token'); //Not admin
+  } else {
+    res.status(200).send(USERS);
+  }
 };
--- backEnd/routers/apiRouter.js
+++ backEnd/routers/apiRouter.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { getInfo } = require('../controller/api.js');
+const { getInfo, getUsersDB } = require('../controller/api.js');
 const { validateTokenMiddleware } = require('../middlewares/validators.js');
 
 router.get('/information', validateTokenMiddleware, getInfo); //Access user's information, Required: header: {Authorization: "Bearer -access token-"}
+router.get('/users', validateTokenMiddleware, getUsersDB); //Get users DB (admin only) by access token header | Required: header: {Authorization: "Bearer -access token-"}
 
 module.exports = router;
